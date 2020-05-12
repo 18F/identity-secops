8bd09f60091b2df5d77e727e2367b2083cf12442
timothy-spencer <timothy.spencer@gsa.gov>
2020-05-12 10:30:53 -0700

    clean up team creation script to handle different cluster types

--- scripts/create-team.sh
+++ scripts/create-team.sh
@@ -3,15 +3,24 @@
 set -o errexit
 
 : ${1?"Usage: $0 <TEAM NAME>"}
+if [ -z "$2" ] ; then
+	CLUSTER_TYPE="cluster"
+else
+	if [ ! -d "${REPO_ROOT}/$2" ]; then
+		echo "no cluster type $2, exiting"
+		exit 1
+	fi
+	CLUSTER_TYPE="$2"
+fi
 
 TEAM_NAME=$1
 TEMPLATE="idp"
 REPO_ROOT=$(git rev-parse --show-toplevel)
-TEAM_DIR="${REPO_ROOT}/cluster/${TEAM_NAME}/"
+TEAM_DIR="${REPO_ROOT}/$CLUSTER_TYPE/${TEAM_NAME}/"
 
 mkdir -p ${TEAM_DIR}
 
-cp -r "${REPO_ROOT}/cluster/${TEMPLATE}/." ${TEAM_DIR}
+cp -r "${REPO_ROOT}/$CLUSTER_TYPE/${TEMPLATE}/." ${TEAM_DIR}
 
 for f in ${TEAM_DIR}*.yaml
 do
@@ -23,5 +32,5 @@ do
 done
 
 echo "${TEAM_NAME} created at ${TEAM_DIR}"
-echo "  - ./${TEAM_NAME}/" >> "${REPO_ROOT}/cluster/kustomization.yaml"
-echo "${TEAM_NAME} added to ${REPO_ROOT}/cluster/kustomization.yaml"
+echo "  - ./${TEAM_NAME}/" >> "${REPO_ROOT}/$CLUSTER_TYPE/kustomization.yaml"
+echo "${TEAM_NAME} added to ${REPO_ROOT}/$CLUSTER_TYPE/kustomization.yaml"
